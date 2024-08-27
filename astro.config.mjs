0cc534c2457e95279d12d09f302dff29e25a7d38
daveFromGrogtag <dave@grogtag.com>
2024-08-27 09:28:03 -0700

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://daveFromGrogtag.github.io',
+  base: 'MIS-Pro',
   integrations: [react()]
 });
